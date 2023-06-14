6c548d850288c65c561e929f93cc5cb755c99c97
Manuela Hernadez <manu.hdz.0795@gmail.com>
2023-06-13 23:24:32 -0500

    Se agrega funciones a los botones comprar productos y confirmar,funcion isvalid para returnar booleano

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const seccionFormularioPago = document.getElementById("seccion_formulario_pago");
+const SeccionPaginaPrincipal = document.getElementById("seccion_pagina_principal");
+const botonComprarComedero =document.getElementById ("comprar_comedero");
+const botonComprarKit = document.getElementById("comprar_kit");
+const botonComprarTunel = document.getElementById("comprar_tunel");
+
+function enviarFormularioPago () {
+    seccionFormularioPago.style.display = "block"
+    SeccionPaginaPrincipal.style.display = "none"
+};
+
+botonComprarComedero.addEventListener("click", function () {
+    enviarFormularioPago()
+});
+
+botonComprarKit.addEventListener("click", function () {
+    enviarFormularioPago()
+});
+
+
+botonComprarTunel.addEventListener("click", function () {
+    enviarFormularioPago()
+});
+
+const botonConfirmar = document.getElementById ("boton_formulario_pago");
+botonConfirmar.addEventListener("click" , function () {
+    let numeroTarjeta = document.getElementById("numero_tarjeta").value;
+    let tarjetaValida = validator.isValid (numeroTarjeta);
+
+    if (tarjetaValida) {
+        alert ("Tarjeta valida ")
+    } else {
+        alert ("Tarjeta invalida ")
+    }
+});
+    
+
+
+
+ 
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
 const validator = {
-  // ...
+  isValid: isValid,
+  maskify: maskify
 };
+function isValid (numeroTarjeta) {
+  let arr = Array.from(numeroTarjeta);
+    arr.reverse();
+    let pares = 0
+    let impares = 0
+    for ( let i = 1; i <= arr.length; i++ ) {
+        console.log("Iteracion: ", i)
+        if(i%2 === 0) {
+            let suma = 0
+            let xyz = arr[i-1] * 2
+            console.log("let:xyz ", xyz)
+            if (xyz >= 10) {
+                let resultado = xyz.toString();
+                console.log ("resultado: ", resultado);
+                for ( let j = 0; j < resultado.length; j++) {
+                  suma = suma + Number(resultado[j]);
+                  console.log ("suma: ", suma);
+                }
+            } else {
+                suma = xyz
+                console.log ("suma=xyz: ", suma)
+            }
+        
+            pares = pares + suma 
+            console.log("pares: ", pares)
+            
+        } else {
+            let posicimpar = arr[i-1]
+            impares = impares + Number(posicimpar);
+            console.log("posicionimpar: ", posicimpar)
+            console.log("impares: ", impares)
+        }
 
+    }
+    let valtotal = impares + pares
+    if (valtotal%2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function maskify (numeroTarjeta) {
+
+}
 export default validator;
+
+
+
